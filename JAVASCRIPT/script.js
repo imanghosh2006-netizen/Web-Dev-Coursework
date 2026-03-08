89849fce7c0e2ab234198826f4e5dcc9d926cb16
Iman Ghosh <imanghosh2006@gmail.com.com>
2026-03-08 15:36:20 +0530

    Updated JS File

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,1 +1,14 @@
 alert("Hello World");
+
+console.log("This is a message in the console.");
+
+// var a=prompt("Enter a number:");
+// console.log("You entered: " + a);
+
+var isTrue = confirm("Do You wanna close this page?")
+if(isTrue){
+        window.close();
+    }
+    else{
+        alert("You chose to stay on the page.");
+    }
