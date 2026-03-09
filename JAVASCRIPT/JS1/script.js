2c606ede56a2514c0ec2d60279a19391704662f4
Iman Ghosh <imanghosh2006@gmail.com.com>
2026-03-10 00:03:37 +0530

    Updated JS1 and Added JS2 files

--- JAVASCRIPT/JS1/script.js
+++ JAVASCRIPT/JS1/script.js
@@ -12,3 +12,7 @@ if(isTrue){
     else{
         alert("You chose to stay on the page.");
     }
+
+document.title = "I am a new title"; // This line changes the title of the webpage.
+
+document.body.style.backgroundColor = "lightblue"; // This line changes the background color of the webpage to light blue.
